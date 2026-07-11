e27408bcdfd92735dd0f6ce89f117d46686ee12c
codebysuruchi <suruchijha320@gmail.com>
2026-07-12 03:30:55 +0530

    first commit

---
+++ Backend/gateway/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import proxy from "express-http-proxy";
+
+dotenv.config();
+
+const port=process.env.PORT
+
+const app=express();
+
+app.use("/auth",proxy(process.env.AUTH_SERVICE))
+
+app.get('/',(req,res)=>{
+    res.json({message: "Gateway server is running"});
+});
+
+app.listen((port),()=>{
+    console.log(`Gateway server is running on port ${port}`);
+});
---
+++ Backend/services/auth/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+
+dotenv.config();
+
+const port=process.env.PORT
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.json({message: "Hello from auth services"});
+});
+
+app.listen((port),()=>{
+    console.log(`services auth server is running on port ${port}`);
+    connectDb();
+});
